c7d0e59372af09baa2368e6b86e6f099c8e40612
leeuihyun <meta212121@naver.com>
2022-02-09 00:28:00 +0900

    기능개발

--- highKit/lee/stackAndQueue/first.cpp
+++ highKit/lee/stackAndQueue/first.cpp
@@ -1,36 +1,41 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
+queue <int> q;
+
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue <int> q;
-    int number;
-    for(int i=0;i<speeds.size();i++){
-        number = 100-progresses[i];
-        if(number%speeds[i]>0){
-            number = number/speeds[i]+1;
-        }
-        else if(number%speeds[i]==0){
-            number = number/speeds[i];
-        }
-        q.push(number);
-    }
-    int res = 0;
-    int fn = q.front();
-    while(!q.empty()){
-        if(fn >= q.front()){
-            q.pop();
-            res+=1;
+    for(int i=0;i<progresses.size();i++){
+        if((100-progresses[i])%speeds[i] == 0) {
+            q.push((100-progresses[i])/speeds[i]);
         }
         else{
-            answer.push_back(res);
-            fn = q.front();
-            res = 0;
+            q.push((100-progresses[i])/speeds[i] + 1);
         }
     }
-    answer.push_back(res);
+    int number = -1;
+    int plus = 1;
+    while(!q.empty()){
+        if(number==-1){
+            number = q.front();
+            q.pop();
+        }
+        else{
+            if(number >= q.front()){
+                plus+=1;
+            }
+            else{
+                answer.push_back(plus);
+                number = q.front();
+                plus = 1;
+            }
+            q.pop();
+        }
+    }
+    answer.push_back(plus);
     return answer;
 }
