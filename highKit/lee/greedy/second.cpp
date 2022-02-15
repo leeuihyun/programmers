6397c82f03a65ac0a1bd6cbb4cab3fceed97925c
leeuihyun <meta212121@naver.com>
2022-02-15 19:16:34 +0900

    그리디

---
+++ highKit/lee/greedy/second.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    int len = name.length();
+    int move = len-1;
+    for(int i=0;i<name.length();i++){
+        answer+=min(name[i]-'A', 'Z'-name[i]+1);
+    }
+    int next = 0;
+    for(int i = 0;i<name.length();i++){
+        next = i+1;
+        while(name[next]=='A' && next<name.length()){
+            next+=1;
+            
+        }
+         move = min(move, i+len-next + min(i,len-next));
+    }
+   
+    return answer+move;
+}
