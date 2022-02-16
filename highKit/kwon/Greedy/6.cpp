2db6b6345ecd87e7627c4c4547c8494522dcf26c
SeungJun-Kwon <80217301+SeungJun-Kwon@users.noreply.github.com>
2022-02-16 23:35:39 +0900

    HighKit_Greedy_6_KSJ

--- highKit/kwon/Greedy/6.cpp
+++ highKit/kwon/Greedy/6.cpp
@@ -4,50 +4,26 @@
 
 using namespace std;
 
-int chk[100] = { 0, };
-
-int getParent(int node) {
-    if(chk[node] == node) return node;
-    return getParent(chk[node]);
-}
-
-void connect(int node1, int node2) {
-    node1 = getParent(node1);
-    node2 = getParent(node2);
-    if(node1 < node2)
-        chk[node2] = node1;
-    else
-        chk[node1] = node2;
-}
-
-bool isCycle(int node1, int node2) {
-    node1 = getParent(node1);
-    node2 = getParent(node2);
-    if(node1 == node2)
-        return true;
-    return false;
-}
-
-int solution(int n, vector<vector<int>> costs) {
-    int answer = 0;
+int solution(vector<vector<int>> routes) {
+    int answer = 0, location;
     
     struct {
         bool operator()(vector<int> a, vector<int> b) {
-            return a[2] < b[2];
+            return a[1] < b[1];
         }
-    } compare;
+    } cmp;
     
-    sort(costs.begin(), costs.end(), compare);
+    sort(routes.begin(), routes.end(), cmp);
     
-    for(int i = 0; i < n; i++)
-        chk[i] = i;
+    location = routes[0][1];
     
-    for(int i = 0; i < costs.size(); i++) {
-        if(!isCycle(costs[i][0], costs[i][1])) {
-            answer += costs[i][2];
-            connect(costs[i][0], costs[i][1]);
+    for(int i = 1; i < routes.size(); i++) {
+        if(routes[i][0] > location) {
+            answer++;
+            location = routes[i][1];
         }
     }
+    answer++;
     
     return answer;
 }
